f4560c27e4f3d986244cfeaeedd1e67404d78228
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-07-27 18:59:17 -0300

    feat: concret separator function implement

--- lib/csv_data.dart
+++ lib/csv_data.dart
@@ -1,32 +1,5 @@
-
-import 'delimited_data.dart';
+import 'package:facul/delimited_data.dart';
 
 class CsvData extends DelimitedData {
-@override
-String separator() {
-    // TODO: implement separator
-    throw UnimplementedError();
-  }
-  void load(String fileName) {
-    // TODO: implement load
-  }
-  void save(String fileName) {
-    // TODO: implement save
-  }
-  void clear() {
-    // TODO: implement clear
-  }
-  bool hasData() {
-    // TODO: implement hasData
-    throw UnimplementedError();
-  }
-  String idata() {
-    // TODO: implement data
-    throw UnimplementedError();
-  }
-  List<String> fields() {
-    // TODO: implement fields
-    throw UnimplementedError();
-  }
-
+  @override String separator() => ',';
 }
