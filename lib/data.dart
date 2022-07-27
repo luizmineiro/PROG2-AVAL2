f98cb1365a8c68081eebc15dfe4c3a0a96074b34
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-07-27 18:56:55 -0300

    refactor: change file variable name

--- lib/data.dart
+++ lib/data.dart
@@ -2,19 +2,19 @@ import 'dart:io';
 
 // ! Verificar a diferença entra load/save e get data/set data
 abstract class Data {
-  late final File file;
+  late final File _file;
 
-  void load(String fileName) => file = File(fileName);
+  void load(String fileName) => _file = File(fileName);
 
-  void save(String fileName) => file.writeAsStringSync(fileName);
+  void save(String fileName) => _file.writeAsStringSync(fileName);
 
-  void clear() => file.writeAsStringSync('');
+  void clear() => _file.writeAsStringSync('');
 
-  bool hasData() => file.lengthSync() > 0;
+  bool hasData() => _file.lengthSync() > 0;
 
-  String get data => file.readAsStringSync(); // Funciona
+  String get data => _file.readAsStringSync(); // Funciona
 
-  set data(value) => file = File(value);
+  set data(value) => _file = File(value);
 
   List<String> fields() => data.split("\n");
 }
